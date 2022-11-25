66f39ada6045b955661b827e6dc816c2b0177ed7
chahahyeon <112802649+chahahyeon@users.noreply.github.com>
2022-11-25 22:45:40 +0900

    Update button.py

--- button.py
+++ button.py
@@ -267,3 +267,20 @@ oppo_image.grid(row=3, column=3)
 turn_result.grid(row=4, column=2)
 # GUI화면 루프처리
 main_window.mainloop()
+
+#로그인하는 프로그램 만들어보기
+from tkinter import *
+window = Tk()
+label1 = Label(window, text="로그인 하세요!!!", font=("Helvetica", 20))
+label1.pack()
+label2 = Label(window, text="아이디")
+label2.pack()
+entry1 = Entry(window)
+entry1.pack()
+label2 = Label(window, text="패스워드")
+label2.pack()
+entry2 = Entry(window)
+entry2.pack()
+button1 = Button(window, text="로그인")
+button1.pack()
+window.mainloop()
